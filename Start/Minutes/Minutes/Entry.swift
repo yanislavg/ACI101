96d4bb86812d2c368aa5d0a0a75273903a72742b
ygeorgiev <ygeorgiev@activtrades.com>
2019-04-03 16:38:24 +0300

    There

--- Start/Minutes/Minutes/Entry.swift
+++ Start/Minutes/Minutes/Entry.swift
@@ -3,7 +3,7 @@ import Foundation
 class Entry: Codable
 {
     var title:   String = "Hello"
-    var content: String = ""
+    var content: String = "There"
     
     var id: String = UUID().uuidString
     
